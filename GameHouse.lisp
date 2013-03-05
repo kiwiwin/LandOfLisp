d6e269200fc4dca5def7ed99f1e3da61aece2e12
kiwi <kiwi.swhite.coder@gmail.com>
2013-03-05 18:29:29 +0800

    reformat

--- GameHouse.lisp
+++ GameHouse.lisp
@@ -1,17 +1,17 @@
 (defparameter *nodes* '((living-room (you are in the living-room. 
-												a wizard is snoring loudly on the couch.))
-												(garden (you are in a beautiful garden.
-												there is a well in front of you.))
-												(attic (you are in the attic.
-												there is a giant welding torch in the corner))))
+                        a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                        there is a well in front of you.))
+                        (attic (you are in the attic.
+                        there is a giant welding torch in the corner))))
 
 (defun describe-location (location nodes)
 (cadr (assoc location nodes)))
 
 (defparameter *edges* '((living-room (garden west door)
-																	 (attic upstairs ladder))
-											(garden (living-room east door))
-											(attic (living-room downstairs ladder))))
+                        (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
 
 (defun describe-path (edge)
 `(there is a ,(caddr edge) going ,(cadr edge) from here.))
@@ -22,18 +22,18 @@
 (defparameter *objects* '(whiskey bucket frog chain))
 
 (defparameter *object-locations* '((whiskey living-room)
-																	 (bucket living-room)
-																	 (chain garden)
-																	 (frog garden)))
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
 
 (defun objects-at (loc objs obj-locs)
 (labels ((at-loc-p (obj)
-					(eq (cadr (assoc obj obj-locs)) loc)))
+         (eq (cadr (assoc obj obj-locs)) loc)))
 (remove-if-not #'at-loc-p objs)))
 
 (defun describe-objects (loc objs obj-loc)
 (labels ((describe-obj (obj)
-					`(you see a ,obj on the floor.)))
+         `(you see a ,obj on the floor.)))
 (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 
 
@@ -41,14 +41,14 @@
 
 (defun look() 
 (append (describe-location *location* *nodes*)
-				(describe-paths *location* *edges*)
-				(describe-objects *location* *objects* *object-locations*)))
+        (describe-paths *location* *edges*)
+        (describe-objects *location* *objects* *object-locations*)))
 
-(defun walk(direction)
+(defun walk (direction)
 (let ((next (find direction 
-									(cdr (assoc *location* *edges*))
-									:key #'cadr)))
+                (cdr (assoc *location* *edges*))
+                :key #'cadr)))
 (if next
-	(progn (setf *location* (car next))
-				 (look))
-	'(you cannot go that way))))
+    (progn (setf *location* (car next))
+           (look))
+    '(you cannot go that way))))
